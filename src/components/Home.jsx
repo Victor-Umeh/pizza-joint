1a3b93d988a3dca307865a9ff226923735abf857
vk196 <victorumeh196@gmail.com>
2023-08-09 12:29:39 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const Home = () => {
+  return (
+    <motion.div className="home container" animate={{}}>
+      <motion.h2 animate={{}} whileHover={{}}>
+        Welcome to Pizza Joint
+      </motion.h2>
+      <Link to="/base">
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
+      </Link>
+    </motion.div>
+  );
+};
+
+export default Home;
