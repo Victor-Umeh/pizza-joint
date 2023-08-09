fe897171847a0254eb64f0abe6d676f6e19a3b9a
vk196 <victorumeh196@gmail.com>
2023-08-09 16:42:59 +0100

    orders page

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,15 +1,46 @@
 import { motion } from "framer-motion";
-
+// import { useAnimationVariants } from "../animationContext";
+const container = {
+  hidden: {
+    x: "100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      mass: 0.4,
+      damp: 8,
+      staggerChildren: 2,
+      when: "beforeChildren",
+    },
+  },
+};
+const childrenVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 const Order = ({ pizza }) => {
+  // const { container, button } = useAnimationVariants();
   return (
-    <div className="container order">
+    <motion.div
+      className="container order"
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
       <h2>Thank you for your order :)</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
-      ))}
-    </div>
+      <motion.div variants={childrenVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 };
 
