94fa1e7ea8433a91050e3bcfd36e03923be13feb
vk196 <victorumeh196@gmail.com>
2023-08-10 21:12:00 +0100

    added svg animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,59 @@
-import { motion } from "framer-motion";
+import { animate, motion } from "framer-motion";
 
+const svgVariants = {
+  From: {
+    rotate: 180,
+    opacity: 0,
+  },
+  To: {
+    rotate: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const pathVariants = {
+  From: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  To: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 3,
+    },
+  },
+};
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg
+        <motion.svg
+          variants={svgVariants}
+          initial="From"
+          animate="To"
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 100 100"
         >
-          <path
+          <motion.path
+            variants={pathVariants}
+            initial="From"
+            animate="To"
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
           />
-          <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
-        </svg>
+          <motion.path
+            variants={pathVariants}
+            initial="From"
+            animate="To"
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+          />
+        </motion.svg>
       </div>
       <motion.div
         className="title"
